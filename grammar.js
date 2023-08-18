8d796f0aff46193c613a018036eda0f6235f9821
Benjamin Van Treese <vantreeseba@gmail.com>
2023-08-18 14:58:12 -0500

    fix: Fix handling of else in conditional statements.

--- grammar.js
+++ grammar.js
@@ -1,11 +1,11 @@
 // const types = require("./grammar-types");
-const operators = require("./grammar-operators.js");
-const literals = require("./grammar-literals.js");
-const declarations = require("./grammar-declarations");
+const operators = require('./grammar-operators.js');
+const literals = require('./grammar-literals.js');
+const declarations = require('./grammar-declarations');
 const keywords = require('./grammar-keywords');
 const builtins = require('./grammar-builtins');
 
-const {commaSep} = require('./utils')
+const { commaSep } = require('./utils');
 
 const preprocessor_statement_start_tokens = ['if', 'elseif'];
 const preprocessor_statement_end_tokens = ['else', 'end'];
@@ -15,12 +15,10 @@ const haxe_grammar = {
   word: ($) => $.identifier,
   inline: ($) => [$.statement, $.expression],
   extras: ($) => [$.comment, /[\s\uFEFF\u2060\u200B\u00A0]/],
-  supertypes: ($) => [
-    $.declaration,
-  ],
+  supertypes: ($) => [$.declaration],
   precedences: ($) => [
     [$._unaryOperator, $._binaryOperator],
-    [$.runtime_type_check_expression, $.pair]
+    [$.runtime_type_check_expression, $.pair],
   ],
   conflicts: ($) => [
     [$.block, $.object],
@@ -39,18 +37,19 @@ const haxe_grammar = {
     statement: ($) =>
       // Use prec.left to favor rules that end SOONER
       // since we have optional semi, this means a semicolon ends the statement.
-      prec.left(seq(
-        choice(
-          $.preprocessor_statement,
-          $.import_statement,
-          $.package_statement,
-          $.declaration,
-          $.expression,
-          $.conditional_statement,
-          $.block,
+      prec.left(
+        seq(
+          choice(
+            $.preprocessor_statement,
+            $.import_statement,
+            $.package_statement,
+            $.declaration,
+            $.expression,
+            $.conditional_statement,
+            $.block,
+          ),
+          optional($._semicolon),
         ),
-        optional($._semicolon)
-      )
       ),
 
     preprocessor_statement: ($) =>
@@ -62,37 +61,38 @@ const haxe_grammar = {
               token.immediate(choice(...preprocessor_statement_start_tokens)),
               $.statement,
             ),
-            token.immediate(choice(...preprocessor_statement_end_tokens))
-          )
-        )
+            token.immediate(choice(...preprocessor_statement_end_tokens)),
+          ),
+        ),
       ),
 
     package_statement: ($) =>
       seq(
         alias('package', $.keyword),
         field('name', $._lhs_expression),
-        $._semicolon
+        $._semicolon,
       ),
 
-    import_statement: ($) => seq(
-      alias('import', $.keyword),
-      field('name', $._lhs_expression),
-      $._semicolon
-    ),
+    import_statement: ($) =>
+      seq(
+        alias('import', $.keyword),
+        field('name', $._lhs_expression),
+        $._semicolon,
+      ),
 
-    _rhs_expression: ($) => choice(
-      $._literal,
-      $.identifier,
-      $.member_expression,
-      $.call_expression
-    ),
+    _rhs_expression: ($) =>
+      choice($._literal, $.identifier, $.member_expression, $.call_expression),
 
-    _unaryExpression: ($) => prec.left(1, choice(
-      // unary on LHS
-      seq($.operator, $._rhs_expression),
-      // unary on RHS
-      seq($._rhs_expression, $.operator),
-    )),
+    _unaryExpression: ($) =>
+      prec.left(
+        1,
+        choice(
+          // unary on LHS
+          seq($.operator, $._rhs_expression),
+          // unary on RHS
+          seq($._rhs_expression, $.operator),
+        ),
+      ),
 
     runtime_type_check_expression: ($) =>
       prec(2, seq('(', $.expression, ':', $.type, ')')),
@@ -105,85 +105,88 @@ const haxe_grammar = {
           '(',
           $._rhs_expression,
           optional(seq(',', field('type', $.type))),
-          ')'
-        )
+          ')',
+        ),
       ),
 
-    _parenthesized_expression: $ => seq(
-      '(',
-      repeat1(prec.left($.expression)),
-      ')'
-    ),
+    _parenthesized_expression: ($) =>
+      seq('(', repeat1(prec.left($.expression)), ')'),
 
-    expression: ($) => choice(
-      $._unaryExpression,
-      $.subscript_expression,
-      $.runtime_type_check_expression,
-      $.cast_expression,
-      $._parenthesized_expression,
-      // simple expression, or chained.
-      seq(
-        $._rhs_expression,
-        repeat(seq($.operator, $._rhs_expression)),
+    expression: ($) =>
+      choice(
+        $._unaryExpression,
+        $.subscript_expression,
+        $.runtime_type_check_expression,
+        $.cast_expression,
+        $._parenthesized_expression,
+        // simple expression, or chained.
+        seq($._rhs_expression, repeat(seq($.operator, $._rhs_expression))),
       ),
-    ),
 
-    subscript_expression: $ =>
-      prec.right(1,
-        seq(
-          $.expression,
-          '[', field('index', $.expression), ']'
-        )
-      ),
+    subscript_expression: ($) =>
+      prec.right(1, seq($.expression, '[', field('index', $.expression), ']')),
 
     member_expression: ($) =>
       prec.right(
         seq(
           field('object', $.identifier),
           token('.'),
-          repeat1(field('member', $._lhs_expression))
-        )
+          repeat1(field('member', $._lhs_expression)),
+        ),
       ),
 
     _lhs_expression: ($) => prec(1, choice($.identifier, $.member_expression)),
 
     builtin_type: ($) => prec.right(choice(...builtins)),
-    type: ($) => prec.right(seq(
-      choice(
-        field('type_name', $._lhs_expression),
-        field('built_in', alias($.builtin_type, $.identifier))
+    type: ($) =>
+      prec.right(
+        seq(
+          choice(
+            field('type_name', $._lhs_expression),
+            field('built_in', alias($.builtin_type, $.identifier)),
+          ),
+          optional($.type_params),
+        ),
       ),
-      optional($.type_params)
-    )),
 
     block: ($) => seq('{', repeat($.statement), '}'),
 
-    metadata: ($) => seq(
-      choice(token('@'), token('@:')),
-      field('name', $._lhs_expression),
-      optional(seq('(', $.expression, ')'))
-    ),
+    metadata: ($) =>
+      seq(
+        choice(token('@'), token('@:')),
+        field('name', $._lhs_expression),
+        optional(seq('(', $.expression, ')')),
+      ),
 
     // arg list is () with any amount of expressions followed by commas
     _arg_list: ($) => seq('(', commaSep($.expression), ')'),
 
-    conditional_statement: ($) => prec(1,
+    conditional_statement: ($) =>
+      prec.right(
+        1,
+        seq(
+          field('name', $.keyword),
+          field('arguments_list', $._arg_list),
+          optional($.block),
+          optional(seq(alias('else', $.keyword), $.block)),
+        ),
+      ),
+
+    _call: ($) =>
+      prec(
+        1,
+        seq(
+          field('object', $._lhs_expression),
+          optional($.type_params),
+          field('arguments_list', $._arg_list),
+        ),
+      ),
+
+    _constructor_call: ($) =>
       seq(
-        field('name', $.keyword),
-        field('arguments_list', $._arg_list)
-      )
-    ),
-
-    _call: ($) => prec(1, seq(
-      field('object', $._lhs_expression),
-      optional($.type_params),
-      field('arguments_list', $._arg_list)
-    )),
-
-    _constructor_call: ($) => seq(
-      optional(alias('new', $.keyword)), // for constructor calls.
-      $._call
-    ),
+        optional(alias('new', $.keyword)), // for constructor calls.
+        $._call,
+      ),
 
     call_expression: ($) => choice($._call, $._constructor_call),
 
@@ -193,7 +196,7 @@ const haxe_grammar = {
 
     comment: ($) =>
       token(
-        choice(seq('//', /.*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))
+        choice(seq('//', /.*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/')),
       ),
     keyword: ($) => prec.right(choice(...keywords)),
     identifier: ($) => /[a-zA-Z_]+[a-zA-Z0-9]*/,
